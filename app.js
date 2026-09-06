2279131dd0d8fba083230f2dcb498f9cb206d463
Ravinder Singh <ravindersanghera2005@gmail.com>
2026-09-06 13:44:43 +0530

    Add homepage redirect

--- app.js
+++ app.js
@@ -83,6 +83,9 @@ app.use((req,res,next)=>{
     res.locals.currUser=req.user;
     next();
 });
+app.get("/", (req, res) => {
+    res.redirect("/listings");
+});
 
 app.use('/listings', listingRouter);
 app.use('/listings/:id/reviews', reviewRouter);
